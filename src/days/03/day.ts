105b9c0c159f2f3b3fee24c5c32f72085e293cb6
Andrew Brown <54123841+brownandrew15@users.noreply.github.com>
2024-12-03 19:24:56 +0000

    Completed Day 3 part 2

--- src/days/03/day.ts
+++ src/days/03/day.ts
@@ -28,6 +28,43 @@ export default class Day03 extends Day {
   }
 
   partTwo (input: string): string {
-    return "";
+    const re = /(mul\(\d+,\d+\))|(do(n't)?\(\))/g;
+    const matches = input.match(re);
+
+    let commands: string[] = [];
+
+    if (matches) {
+      commands = matches;
+    }
+
+    let includeCommand = true;
+
+    const filteredCommands = commands.filter(value => {
+      const mulCommand = /mul\(\d+,\d+\)/;
+      const doCommand = /do\(\)/
+      const dontCommand = /don't\(\)/
+
+      if (mulCommand.test(value)) {
+        return includeCommand;
+      } else {
+        if (doCommand.test(value)) {
+          includeCommand = true;
+        } else if (dontCommand.test(value)) {
+          includeCommand = false;
+        }
+        return false;
+      }
+
+    });
+    
+    const values = filteredCommands.map((value) => {
+      const re = /\d+/g;
+      const matches = value.match(re);
+      const left = parseInt(matches![0]);
+      const right = parseInt(matches![1]);
+      return left * right;
+    });
+
+    return values.reduce((value1, value2) => value1 + value2, 0).toString();
   }
 }
