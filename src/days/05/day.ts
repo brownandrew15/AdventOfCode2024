4d1620092b051802ea8ec1f7817065195abf89b9
Andrew Brown <54123841+brownandrew15@users.noreply.github.com>
2024-12-07 20:58:17 +0000

    Completed Day 5 part 2

--- src/days/05/day.ts
+++ src/days/05/day.ts
@@ -14,31 +14,31 @@ export default class Day05 extends Day {
     return rulesString.split("\n").map(pair => pair.split("|").map(value => parseInt(value)));
   }
 
+  createRulesMapping(rulePairs: number[][]) {
+    let rules = new Map<number, number[]>();
+    rulePairs.forEach(pair => {
+      if (!(rules.has(pair[1]))) {
+        rules.set(pair[1], []);
+      }
+      rules.get(pair[1])?.push(pair[0]);
+    });
+    return rules;
+
+  }
+
   isUpdateValid(update: number[], printRules: Map<number, number[]>) {
     let rtnValue = true;
     update.forEach(value => {
-      const successors = printRules.get(value);
-      if (successors) {
-        const successorsInUpdate = successors.filter(successor => update.includes(successor));
-        const validSuccessorsInUpdate = successorsInUpdate.filter(successor => update.indexOf(successor) > update.indexOf(value));
-        if (!(validSuccessorsInUpdate.length == successorsInUpdate.length)) {
+      const predecessors = printRules.get(value);
+      if (predecessors) {
+        const predecessorsInUpdate = predecessors.filter(successor => update.includes(successor));
+        const validPredecessorsInUpdate = predecessorsInUpdate.filter(successor => update.indexOf(successor) < update.indexOf(value));
+        if (!(validPredecessorsInUpdate.length == predecessorsInUpdate.length)) {
           rtnValue = false;
         }
       }
     });
     return rtnValue;
-  }
-
-  createRulesMapping(rulePairs: number[][]) {
-    let rules = new Map<number, number[]>();
-    rulePairs.forEach(pair => {
-      if (!(rules.has(pair[0]))) {
-        rules.set(pair[0], []);
-      }
-      rules.get(pair[0])?.push(pair[1]);
-    });
-    return rules;
-
   }
 
   filterRules(update: number[], rules: number[][]): number[][] {
@@ -68,7 +68,44 @@ export default class Day05 extends Day {
     return centerValues.reduce((value1, value2) => value1 + value2).toString();
   }
 
+
+  sortUpdate(update: number[], ruleMapping: Map<number, number[]>): number[] {
+    const newUpdate = update.map(update => update);
+    let changed = true;
+    while (changed) {
+      let changedOnPass = false;
+      for (let i=1; i < newUpdate.length; i++) {
+        if (ruleMapping.get(newUpdate[i-1])?.includes(newUpdate[i])) {
+          const temp = newUpdate[i];
+          newUpdate[i] = newUpdate[i-1];
+          newUpdate[i-1] = temp;
+          changedOnPass = true;
+        }
+      }
+      changed = changedOnPass
+    }
+    return newUpdate;
+  }
+
   partTwo (input: string): string {
-    return "";
+    const inputSections = input.split("\n\n");
+    const rules = this.getRules(inputSections[0]);
+    const updates = this.getUpdates(inputSections[1]);
+
+    const correctedUpdates = updates.map(update => {
+      const ruleMapping = this.createRulesMapping(
+        this.filterRules(update, rules)
+      );
+      if (this.isUpdateValid(update, ruleMapping)) return [];
+
+      return this.sortUpdate(update, ruleMapping);
+
+    }).filter(update => update.length > 0);
+
+    const centerValues = correctedUpdates.map(update => {
+      return update[(update.length - 1) / 2];
+    });
+
+    return centerValues.reduce((value1, value2) => value1 + value2).toString();
   }
 }
